d941cba848de57f623286c53cd964fcea7d8772a
thldesignz <bruce.slaughter@smail.rasmussen.edu>
2021-05-07 01:09:50 -0400

    d
    gggg

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-.postCss('resources/css/app.css', 'public/css', [
-    //
-]);
-    mix.browserSync('http://127.0.0.1:8000');
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
